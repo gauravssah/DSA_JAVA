cba2cd9b6b9bf5868707cbe4def28697f3a7ecba
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-11-11 22:13:37 +0530

    welcome to java

---
+++ 06 JAVA/01_Java/javaBasics.java
@@ -0,0 +1,11 @@
+public class javaBasics {
+    public static void main(String[] args) {
+        System.out.println("Hello Gaurav");
+        System.out.println("Hello Gaurav");
+        System.out.println("Hello Gaurav");
+        System.out.println("Hello Gaurav");
+        System.out.print("Go\n");
+        System.out.print("Go ");
+        System.out.print("Gaurav");
+    }
+}
---
+++ 06 JAVA/02_Print_Pattern/java01.java
@@ -0,0 +1,8 @@
+public class java01 {
+    public static void main(String arg[]) {
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
